34c9c19c4e111231e509b1ba09fc98b12773b5c0
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-03-05 22:30:39 +0700

    Добавьте файлы проекта.

---
+++ Домашнее задание - Методы/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Домашнее_задание___Методы
+{
+    // написать метод который выводит на экран строку.
+    // Символы из которых состоит строка и их колличество вводятся пользователем.
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int  quantity ;
+            string Znak , resault ;
+
+             Znak = Console.ReadLine();
+             quantity = int.Parse(Console.ReadLine());
+            resault = Quantity(Znak, quantity);
+             PrintResault(resault);
+
+        }
+
+        static string Quantity ( string Znak, int A)
+        {
+            return string.Concat(Enumerable.Repeat(Znak, A));
+        }
+       static void PrintResault (string resault) 
+        {
+            Console.WriteLine(resault + "\t");
+        }
+
+    }
+}
